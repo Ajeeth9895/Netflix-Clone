4aaca3162f83b29870db6c5efbff4ddbbadab3ff
Ajee9895 <ajeethkumar.gopal@gmail.com>
2023-08-02 20:59:09 +0530

    happy commit

--- src/helpers/firebase.js
+++ src/helpers/firebase.js
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDDC8AV3zDaTtpxkh2z_cN4kVEmpnL8yMA",
-    authDomain: "netflix-clone-b4b67.firebaseapp.com",
-    projectId: "netflix-clone-b4b67",
-    storageBucket: "netflix-clone-b4b67.appspot.com",
-    messagingSenderId: "115387159502",
-    appId: "1:115387159502:web:cef181de7c8c27a653919f"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
 };
 
 //to use firebase services in application
